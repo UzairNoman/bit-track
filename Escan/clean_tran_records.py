2493ea3a8f78bbc9abf17d8296b42134fb9987bc
Shuo Chen <shuo.chen@campus.lmu.de>
2022-01-11 09:49:25 +0100

    simplify transaction records

---
+++ Escan/clean_tran_records.py
@@ -0,0 +1,27 @@
+"""
+remove useless attributes in transaction records
+"""
+import json
+
+with open("transaction_record.jsonl", "r") as f:
+    for line in f.readlines():
+        record = json.loads(line)
+        for i, tran in enumerate(record["transactions"]):
+            del tran["blockNumber"]
+            del tran["hash"]
+            del tran["nonce"]
+            del tran["blockHash"]
+            del tran["transactionIndex"]
+            del tran["gas"]
+            del tran["gasPrice"]
+            del tran["isError"]
+            del tran["txreceipt_status"]
+            del tran["input"]
+            del tran["contractAddress"]
+            del tran["cumulativeGasUsed"]
+            del tran["gasUsed"]
+            del tran["confirmations"]
+            record["transactions"][i] = tran
+        with open("transaction_record_simplify.jsonl", "a") as f2:
+            record = json.dumps(record)
+            f2.write(record +"\n")
